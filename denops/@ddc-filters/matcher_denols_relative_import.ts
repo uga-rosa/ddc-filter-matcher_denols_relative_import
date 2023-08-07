f2331de61ba390f774dc1dc7380593a2b90fd4ab
uga-rosa <uga6603@gmail.com>
2023-08-07 17:20:57 +0900

    Check deno.suggest.imports.hosts

--- denops/@ddc-filters/matcher_denols_relative_import.ts
+++ denops/@ddc-filters/matcher_denols_relative_import.ts
@@ -5,18 +5,23 @@ import * as u from "https://deno.land/x/unknownutil@v3.4.0/mod.ts";
 type Params = Record<never, never>;
 
 export class Filter extends BaseFilter<Params> {
-  filter({
+  hostByClient: Record<string, string[]> = {};
+
+  async filter({
+    denops,
     items,
   }: FilterArguments<Params>): Promise<Item[]> {
-    return Promise.resolve(items.filter((item) => {
+    return await items.reduce(async (acc, item) => {
+      const items = await acc;
       if (
         !u.isObjectOf({
           user_data: u.isObjectOf({
+            clientId: u.isNumber,
             lspitem: u.isString,
           }),
         })(item)
       ) {
-        return true;
+        return [...items, item];
       }
       const lspitem = JSON.parse(item.user_data.lspitem);
       if (
@@ -28,11 +33,26 @@ export class Filter extends BaseFilter<Params> {
           }),
         })(lspitem)
       ) {
-        return true;
+        return [...items, item];
       }
+
+      const clientId = item.user_data.clientId;
+      if (this.hostByClient[clientId] == null) {
+        const hosts = await (denops.call(
+          "luaeval",
+          `vim.lsp.get_client_by_id(${clientId}).config.init_options.suggest.imports.hosts`,
+        ) as Promise<Record<string, boolean>>).catch(() => ({}));
+        this.hostByClient[clientId] = Object.keys(hosts);
+      }
+      const hosts = this.hostByClient[clientId];
+
       const source = lspitem.data.tsc.source;
-      return !source.startsWith("https://deno.land");
-    }));
+      if (hosts.every((host) => !source.startsWith(host))) {
+        return [...items, item];
+      } else {
+        return items;
+      }
+    }, Promise.resolve([] as Item[]));
   }
 
   params(): Params {
